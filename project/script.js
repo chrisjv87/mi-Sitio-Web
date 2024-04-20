df89c4ebd69dba0dffdbd5e2b5553212011cd382
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-04-20 13:54:04 -0600

    Add file JSON for translations and create functions

--- project/script.js
+++ project/script.js
@@ -1,37 +1,58 @@
-const buttonNext = document.querySelector('.icon-arrow-down');
-const aboutButton = document.querySelector('.about');
-const skillsButton = document.querySelector('.skills');
-const workButton = document.querySelector('.work');
-const trainingButton = document.querySelector('.training');
-const mainContent = document.querySelector('.main-content');
+const homeButton = document.querySelector('#home');
+const aboutButton = document.querySelector('#about');
+const skillsButton = document.querySelector('#skills');
+const workButton = document.querySelector('#work');
+const trainingButton = document.querySelector('#training');
+const mainContent = document.querySelector('#main-content');
+const buttons = document.querySelectorAll('.buttonMenu');
+const darkMode = document.querySelector('#dark-mode');
+const languageButton = document.querySelector('#language'); 
 
-buttonNext.addEventListener('click', loadFile);
-aboutButton.addEventListener('click', () => loadFile(aboutRoute));
-skillsButton.addEventListener('click', () => loadFile(skillsRoute));
-workButton.addEventListener('click', () => loadFile(workRoute));
-trainingButton.addEventListener('click', () => loadFile(trainingRoute));
+homeButton.addEventListener('click', () => routeValidation(indexRoute));
+aboutButton.addEventListener('click', () => routeValidation(aboutRoute));
+skillsButton.addEventListener('click', () => routeValidation(skillsRoute));
+workButton.addEventListener('click', () => routeValidation(workRoute));
+trainingButton.addEventListener('click', () => routeValidation(trainingRoute));
 
+const indexRoute = './index.html';
 const aboutRoute = './components/about.html';
 const skillsRoute = './components/skills.html';
-const workRoute = './components/work.html'; 
+const workRoute = './components/work.html';
 const trainingRoute = './components/training.html';
 
-function loadFile(ruta) {
-    fetch(ruta)
+
+function routeValidation(routeFile) {
+    if(routeFile !== indexRoute) {
+        loadFile(routeFile);
+    } else {
+        loadFile(routeFile)
+        ressetButtonsColor();
+    }
+};
+
+function loadFile(routeFile) {
+    const currentSection = mainContent.getAttribute('id');
+
+    fetch(routeFile)
         .then(response => response.text())
         .then(html => {
             const parser = new DOMParser();
-            const doc = parser.parseFromString(html, 'text/html');
-            const newMainContent = doc.querySelector('main');
-            const currentMainContent = document.querySelector('.main-content');
+            const htmlFile = parser.parseFromString(html, 'text/html');
+            const mainLabel = htmlFile.querySelector('main');
+            const mainGetId = mainLabel.getAttribute('id');
 
-            currentMainContent.classList.add('hide');
+            if(currentSection === mainGetId) {
+                return;
+            }
+
+            mainContent.classList.add('hide');
 
             setTimeout(() => {
-                currentMainContent.innerHTML = newMainContent.innerHTML;
+                mainContent.id = mainGetId;
+                mainContent.innerHTML = mainLabel.innerHTML;
 
                 setTimeout(() => {
-                    currentMainContent.classList.remove('hide');
+                    mainContent.classList.remove('hide');
                 }, 50);
             }, 500);
 
@@ -41,43 +62,73 @@ function loadFile(ruta) {
         });
 };
 
-let totalScrollDown = 0;
-let scrollDirection = 0; // 1 indica scroll hacia abajo, -1 indica scroll hacia arriba
+// Restablecer el color de los botones cuando se cambia al home.
+function ressetButtonsColor() {
+    buttons.forEach(btn => {
+        btn.classList.remove('active')
+    });
+}
 
-window.addEventListener('wheel', scrollDownContent);
+// Borra clase active en todos los botones y agrega active en el presionado.
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        ressetButtonsColor();
+        button.classList.add('active');
+    });
+});
 
-function scrollDownContent(event) {
-    if (event.deltaY > 0) {
-        if (scrollDirection === -1) {
-            totalScrollDown = 0; // Reinicia el conteo si hubo un cambio de dirección
-        }
-        totalScrollDown += event.deltaY;
-        scrollDirection = 1;
-    } else {
-        scrollDirection = -1;
-    }
+// Cambiar idioma de la pagina.
 
-    if (totalScrollDown >= 720) { // 360 es un valor arbitrario para media vuelta de scroll
-        
-        fetch('./about/about.html')
-        .then(response => response.text())
-        .then(html => {
-            const parser = new DOMParser();
-            console.log('parser', parser);
-            const aboutDocument = parser.parseFromString(html, 'text/html');
-            console.log('aboutDocument', aboutDocument);
-            const aboutMain = aboutDocument.querySelector('main.section-content');
-            console.log('aboutMain', aboutMain);
-            const aboutMainClass = aboutMain.getAttribute('class');
+languageButton.addEventListener('click', () => toggleLanguage());
 
-            mainContent.innerHTML = aboutMain.innerHTML;
-            mainContent.className = aboutMainClass;
-        })
-        .catch(error => {
-            console.error('Error al cargar el contenido:', error);
-        });
-        totalScrollDown = 0;
-        scrollDirection = 0;
+let translations;
+let currentLanguage = 'es'; // Define currentLanguage aquí
+
+const translationsUrl = './locales/translations.json';
+
+const loadTranslations = async () => {
+    try {
+        const response = await fetch(translationsUrl);
+        const translationsData = await response.json();
+        translations = translationsData;
+
+        console.log('translationsLoad', translations);
+
+
+        // Llamamos a updateTranslations() después de cargar las traducciones
+        updateTranslations();
+
+    } catch (error) {
+        console.error('Error al cargar las traducciones', error);
     }
 };
 
+
+const toggleLanguage = () => {
+    currentLanguage = currentLanguage === 'es' ? 'en' : 'es';
+    updateTranslations();
+}
+
+const updateTranslations = () => {
+    const elementsToUpdate = document.querySelectorAll('[data-translate]');
+    console.log('elementsToUpdate', elementsToUpdate);
+    elementsToUpdate.forEach(element => {
+        console.log('element', element);
+        const key = element.dataset.translate;
+        const value = translations[key];
+        console.log('key', key);
+        console.log('translationsUPdate', translations);
+        console.log('value', value);
+        if (translations) {
+            const translatedText = translations.index.text.en; 
+            console.log('translatedText', translatedText);
+            element.textContent = translatedText || '';
+            console.log('element.textContent', element.textContent);
+        } else {
+            console.error('Error: Traducciones no cargadas correctamente.');
+        }
+
+    });
+};
+
+window.addEventListener('DOMContentLoaded', loadTranslations);
