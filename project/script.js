cee68c5c7f5bc2f89f85a1389a9082c70ae15041
ChrisValDev <jimenez.valverde0805@gmail.com>
2024-05-28 13:52:04 -0600

    Add functions to translations in all the sections

--- project/script.js
+++ project/script.js
@@ -4,15 +4,19 @@ const skillsButton = document.querySelector('#skills');
 const workButton = document.querySelector('#work');
 const trainingButton = document.querySelector('#training');
 const mainContent = document.querySelector('#main-content');
-const buttons = document.querySelectorAll('.buttonMenu');
 const darkMode = document.querySelector('#dark-mode');
-const languageButton = document.querySelector('#language'); 
+const buttons = document.querySelectorAll('.buttonMenu');
+const languageButton = document.getElementById('language');
+const textsToChange = document.querySelectorAll('[data-section]')
+let currentLanguage = 'es';
+
 
 homeButton.addEventListener('click', () => routeValidation(indexRoute));
 aboutButton.addEventListener('click', () => routeValidation(aboutRoute));
 skillsButton.addEventListener('click', () => routeValidation(skillsRoute));
 workButton.addEventListener('click', () => routeValidation(workRoute));
 trainingButton.addEventListener('click', () => routeValidation(trainingRoute));
+languageButton.addEventListener('click', toogleLanguage);
 
 const indexRoute = './index.html';
 const aboutRoute = './components/about.html';
@@ -21,114 +25,96 @@ const workRoute = './components/work.html';
 const trainingRoute = './components/training.html';
 
 
-function routeValidation(routeFile) {
-    if(routeFile !== indexRoute) {
-        loadFile(routeFile);
+function routeValidation(route) {
+    if(route !== indexRoute) {
+        loadFile(route);
     } else {
-        loadFile(routeFile)
+        loadFile(route)
         ressetButtonsColor();
     }
 };
 
-function loadFile(routeFile) {
-    const currentSection = mainContent.getAttribute('id');
+const changeContent = (texts) => {
+    const elements = document.querySelectorAll('[data-section]');
+    
+    elements.forEach(element => {
+        const section = element.getAttribute('data-section');
+        const value = element.getAttribute('data-value');
+        if(texts[section] && texts[section][value]) {
+            element.innerHTML = texts[section][value];
+        }
+    });
+}
 
-    fetch(routeFile)
+function loadFile(file) {
+    fetch(file)
         .then(response => response.text())
         .then(html => {
             const parser = new DOMParser();
             const htmlFile = parser.parseFromString(html, 'text/html');
             const mainLabel = htmlFile.querySelector('main');
-            const mainGetId = mainLabel.getAttribute('id');
+            mainGetId = mainLabel.id;
 
-            if(currentSection === mainGetId) {
-                return;
+            if (!mainLabel) {
+                throw new Error(`No se encontro la etiqueta <main> en el archivo ${html} cargado`);
             }
-
+        
             mainContent.classList.add('hide');
-
+            mainContent.innerHTML = mainLabel.innerHTML;
+            
             setTimeout(() => {
-                mainContent.id = mainGetId;
-                mainContent.innerHTML = mainLabel.innerHTML;
-
-                setTimeout(() => {
-                    mainContent.classList.remove('hide');
-                }, 50);
-            }, 500);
-
+                mainContent.classList.remove('hide');
+            }, 300);
+                
         })
         .catch(error => {
             console.error('Error al cargar el contenido:', error);
-        });
+        })
 };
+
+
 
 // Restablecer el color de los botones cuando se cambia al home.
 function ressetButtonsColor() {
     buttons.forEach(btn => {
-        btn.classList.remove('active')
-    });
+        btn.classList.remove('active');
+        });
+    changeLanguage(currentLanguage);
+
 }
 
-// Borra clase active en todos los botones y agrega active en el presionado.
 buttons.forEach(button => {
     button.addEventListener('click', () => {
+        
         ressetButtonsColor();
-        button.classList.add('active');
+        
+        button.classList.add('active'); 
+        currentLanguage = languageButton.dataset.language;
+        
+        changeLanguage(currentLanguage);
     });
 });
 
 // Cambiar idioma de la pagina.
 
-languageButton.addEventListener('click', () => toggleLanguage());
-
-let translations;
-let currentLanguage = 'es'; // Define currentLanguage aquí
-
-const translationsUrl = './locales/translations.json';
-
-const loadTranslations = async () => {
-    try {
-        const response = await fetch(translationsUrl);
-        const translationsData = await response.json();
-        translations = translationsData;
-
-        console.log('translationsLoad', translations);
-
-
-        // Llamamos a updateTranslations() después de cargar las traducciones
-        updateTranslations();
-
-    } catch (error) {
-        console.error('Error al cargar las traducciones', error);
-    }
-};
-
-
-const toggleLanguage = () => {
-    currentLanguage = currentLanguage === 'es' ? 'en' : 'es';
-    updateTranslations();
+function toogleLanguage() {
+    const currentLanguage = localStorage.getItem('language');
+    const newLanguage = currentLanguage === 'es' ? 'en' : 'es';
+    languageButton.setAttribute('data-language', newLanguage);
+    localStorage.setItem('language', newLanguage);
+    
+    changeLanguage(newLanguage);
 }
 
-const updateTranslations = () => {
-    const elementsToUpdate = document.querySelectorAll('[data-translate]');
-    console.log('elementsToUpdate', elementsToUpdate);
-    elementsToUpdate.forEach(element => {
-        console.log('element', element);
-        const key = element.dataset.translate;
-        const value = translations[key];
-        console.log('key', key);
-        console.log('translationsUPdate', translations);
-        console.log('value', value);
-        if (translations) {
-            const translatedText = translations.index.text.en; 
-            console.log('translatedText', translatedText);
-            element.textContent = translatedText || '';
-            console.log('element.textContent', element.textContent);
-        } else {
-            console.error('Error: Traducciones no cargadas correctamente.');
-        }
-
-    });
+const changeLanguage = async (lang) => {
+    const requestJson = await fetch(`./locales/${lang}.json`);
+    const texts = await requestJson.json();
+    
+    changeContent(texts)
 };
 
-window.addEventListener('DOMContentLoaded', loadTranslations);
+window.addEventListener('DOMContentLoaded', () => {
+    
+    loadFile(`${indexRoute}`); // Cargar el archivo inicial
+});
+
